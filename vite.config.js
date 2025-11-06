c8e6f27898db38003cc86451584259f5d4525663
yihune1234 <yihunebelay859@gmail.com>
2025-11-06 13:36:20 +0300

    prepare for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: './index.html'
     }
-  },
-  base: './'
+  }
 })
